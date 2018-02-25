a168ad04f875f442d54d93430fcfec7c7c3a77a8
Cameron <camerondsmick@gmail.com>
2018-02-25 15:00:52 -0500

    Incorporated os module to read config vars

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-import requests
+import os
 from groupme_bot import Groupme_bot
 from flask import Flask, json, request
 
 # initialize Flask app
 app = Flask(__name__)
 
-# get config variables
-config = requests.get('https://api.heroku.com/apps/rodney-copperbottom/config-vars').json()
+# load environment variables
+BOT_ID = os.environ['BOT_ID']
+GROUP_ID = os.environ['GROUP_ID']
 
 # instantiate chat bots
-groupme_bot = Groupme_bot(config.BOT_ID)
+groupme_bot = Groupme_bot(BOT_ID)
 
 @app.route("/")
 def hello():
@@ -18,8 +19,7 @@ def hello():
 @app.route('/groupme', methods=['POST'])
 def groupme_callback():
     json_body = request.get_json()
-    group_id = json_body['group_id']
     message = json_body['text']
-    if group_id == config.GROUP_ID and groupme_bot.is_command(message):
+    if json_body['group_id'] == GROUP_ID and groupme_bot.is_command(message):
         command, args = groupme_bot.parse_message(message)
         groupme_bot.send_message("Command: {}\nArgs: {}".format(command, args));
