e6b446726f3e6a14630ee19ee24c4464c08b45be
Cameron <camerondsmick@gmail.com>
2018-02-26 01:22:16 -0500

    Added auth_token for authenticated requests

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 # load environment variables
 BOT_ID = os.environ['BOT_ID']
 GROUP_ID = os.environ['GROUP_ID']
+AUTH_TOKEN = os.environ['AUTH_TOKEN']
 
 # instantiate chat bots
 groupme_bot = Groupme_bot(BOT_ID, GROUP_ID)
--- groupme_bot.py
+++ groupme_bot.py
@@ -20,9 +20,10 @@ class Groupme_bot(object):
         def to_dict(self):
             return self.d
 
-    def __init__(self, bot_id, group_id):
+    def __init__(self, bot_id, group_id, auth_token):
         self.bot_id = bot_id
         self.group_id = group_id
+        self.auth_token = auth_token
         self.POST_URL = 'https://api.groupme.com/v3/bots/post'
         self.GROUP_URL = 'https://api.groupme.com/v3/groups/{}'.format(self.group_id)
         self.functions = {}
@@ -40,9 +41,8 @@ class Groupme_bot(object):
         requests.post(self.POST_URL, json=m)
 
     def notify_all(self):
-        json = requests.get(self.GROUP_URL).json()
-        print(json)
-        members = json['members']
+        auth = {'token':self.auth_token}
+        members = requests.get(self.GROUP_URL, params=auth).json()['members']
         uids = map(lambda x: x['user_id'], members)
         message = Message_builder().mention(uids)
         self.send_message(message)
