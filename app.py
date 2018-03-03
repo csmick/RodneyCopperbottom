7f6779145b73427aff55c507b4a6c1531e922bda
Cameron <camerondsmick@gmail.com>
2018-03-02 22:58:29 -0500

    Fixed curly quote iOS 11 bug

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def hello():
 @app.route('/groupme', methods=['POST'])
 def groupme_callback():
     json_body = request.get_json()
-    message = json_body['text']
+    message = json_body['text'].replace('“','"').replace('”','"')
     if json_body['group_id'] == GROUP_ID:
         if groupme_bot.is_command(message):
             command, args = groupme_bot.parse_message(message)
