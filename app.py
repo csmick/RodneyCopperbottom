e91af74a2b89d3e08a8c24c555e4d638df203c13
Dan Wilborn <dwilborn@nd.edu>
2018-03-16 20:33:17 -0400

    another print statement

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def groupme_callback():
                     spammer = json_body['name']
                     groupme_bot.spammer_berate(spammer, uid)
             else:
-                print("clearing")
+                print("clearing:" len(timestamped_uids))
                 timestamped_uids.clear()
 
     return ''
